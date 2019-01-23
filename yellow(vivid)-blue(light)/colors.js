548b63aadbb8c56fb348531265c69f84295e2a98
maccorzo <marko@hankkila.me>
2019-01-23 20:47:56 +0200

    Add yellow (vivid)- light blue theme

---
+++ yellow(vivid)-blue(light)/colors.js
@@ -0,0 +1,74 @@
+/*
+  Yellow (vivid) / Light blue (vivid) color theme
+  
+  Use:
+  import styled from 'styled-components';
+  import colors from '../path_to_this_file/colors';
+
+  const StyledDiv = styled.div`
+    background: ${color.colorPrimary10};
+    color: ${color.colorGray3};
+    h1 {
+      color: ${color.colorGray1};
+    }
+  `;
+*/
+const colors = {
+  colorPrimary1: 'hsl(15, 86%, 30%)',
+  colorPrimary2: 'hsl(22, 82%, 39%)',
+  colorPrimary3: 'hsl(29, 80%, 44%)',
+  colorPrimary4: 'hsl(36, 77%, 49%)',
+  colorPrimary5: 'hsl(42, 87%, 55%)',
+  colorPrimary6: 'hsl(44, 92%, 63%)',
+  colorPrimary7: 'hsl(48, 94%, 68%)',
+  colorPrimary8: 'hsl(48, 95%, 76%)',
+  colorPrimary9: 'hsl(48, 100%, 88%)',
+  colorPrimary10: 'hsl(49, 100%, 96%)',
+
+  colorBlue1: 'hsl(204, 96%, 27%)',
+  colorBlue2: 'hsl(203, 87%, 34%)',
+  colorBlue3: 'hsl(202, 83%, 41%)',
+  colorBlue4: 'hsl(201, 79%, 46%)',
+  colorBlue5: 'hsl(199, 84%, 55%)',
+  colorBlue6: 'hsl(197, 92%, 61%)',
+  colorBlue7: 'hsl(196, 94%, 67%)',
+  colorBlue8: 'hsl(195, 97%, 75%)',
+  colorBlue9: 'hsl(195, 100%, 85%)',
+  colorBlue10: 'hsl(195, 100%, 95%)',
+
+  colorGray1: 'hsl(0, 0%, 13%)',
+  colorGray2: 'hsl(0, 0%, 23%)',
+  colorGray3: 'hsl(0, 0%, 32%)',
+  colorGray4: 'hsl(0, 0%, 38%)',
+  colorGray5: 'hsl(0, 0%, 49%)',
+  colorGray6: 'hsl(0, 0%, 62%)',
+  colorGray7: 'hsl(0, 0%, 69%)',
+  colorGray8: 'hsl(0, 0%, 81%)',
+  colorGray9: 'hsl(0, 0%, 88%)',
+  colorGray10: 'hsl(0, 0%, 97%)',
+
+  colorSupportRed1: 'hsl(348, 94%, 20%)',
+  colorSupportRed2: 'hsl(350, 94%, 28%)',
+  colorSupportRed3: 'hsl(352, 90%, 35%)',
+  colorSupportRed4: 'hsl(354, 85%, 44%)',
+  colorSupportRed5: 'hsl(356, 75%, 53%)',
+  colorSupportRed6: 'hsl(360, 83%, 62%)',
+  colorSupportRed7: 'hsl(360, 91%, 69%)',
+  colorSupportRed8: 'hsl(360, 100%, 80%)',
+  colorSupportRed9: 'hsl(360, 100%, 87%)',
+  colorSupportRed10: 'hsl(360, 100%, 95%)',
+
+  colorSupportGreen1: 'hsl(170, 97%, 15%)',
+  colorSupportGreen2: 'hsl(168, 80%, 23%)',
+  colorSupportGreen3: 'hsl(166, 72%, 28%)',
+  colorSupportGreen4: 'hsl(164, 71%, 34%)',
+  colorSupportGreen5: 'hsl(162, 63%, 41%)',
+  colorSupportGreen6: 'hsl(160, 51%, 49%)',
+  colorSupportGreen7: 'hsl(158, 58%, 62%)',
+  colorSupportGreen8: 'hsl(156, 73%, 74%)',
+  colorSupportGreen9: 'hsl(154, 75%, 87%)',
+  colorSupportGreen10: 'hsl(152, 68%, 96%)',
+
+};
+
+export default colors;
