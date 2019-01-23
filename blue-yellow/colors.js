bf2d001905035dfab8a346b96a73cc83ba2b263c
Marko Hankkila <marko@hankkila.me>
2019-01-23 20:44:00 +0200

    Add blue&yellow theme (#1)
    * Add readme file
    
    * Add blue-yellow theme

---
+++ blue-yellow/colors.js
@@ -0,0 +1,72 @@
+/*
+  This is a Blue / Yellow color theme
+  Use:
+  import styled from 'styled-components';
+  import colors from '../path_to_this_file/colors';
+
+  const StyledDiv = styled.div`
+    background: ${color.colorPrimary10};
+    color: ${color.colorGray3};
+    h1 {
+      color: ${color.colorGray1};
+    }
+  `;
+*/
+const colors = {
+  colorPrimary1: 'hsl(205, 100%, 21%)',
+  colorPrimary2: 'hsl(205, 87%, 29%)',
+  colorPrimary3: 'hsl(205, 82%, 33%)',
+  colorPrimary4: 'hsl(205, 76%, 39%)',
+  colorPrimary5: 'hsl(205, 67%, 45%)',
+  colorPrimary6: 'hsl(205, 65%, 55%)',
+  colorPrimary7: 'hsl(205, 74%, 65%)',
+  colorPrimary8: 'hsl(205, 84%, 74%)',
+  colorPrimary9: 'hsl(205, 97%, 85%)',
+  colorPrimary10: 'hsl(205, 79%, 92%)',
+
+  colorYellow1: 'hsl(15, 86%, 30%)',
+  colorYellow2: 'hsl(22, 82%, 39%)',
+  colorYellow3: 'hsl(29, 80%, 44%)',
+  colorYellow4: 'hsl(36, 77%, 49%)',
+  colorYellow5: 'hsl(42, 87%, 55%)',
+  colorYellow6: 'hsl(44, 92%, 63%)',
+  colorYellow7: 'hsl(48, 94%, 68%)',
+  colorYellow8: 'hsl(48, 95%, 76%)',
+  colorYellow9: 'hsl(48, 100%, 88%)',
+  colorYellow10: 'hsl(49, 100%, 96%)',
+
+  colorGray1: 'hsl(209, 61%, 16%)',
+  colorGray2: 'hsl(211, 39%, 23%)',
+  colorGray3: 'hsl(209, 34%, 30%)',
+  colorGray4: 'hsl(209, 28%, 39%)',
+  colorGray5: 'hsl(210, 22%, 49%)',
+  colorGray6: 'hsl(209, 23%, 60%)',
+  colorGray7: 'hsl(211, 27%, 70%)',
+  colorGray8: 'hsl(210, 31%, 80%)',
+  colorGray9: 'hsl(212, 33%, 89%)',
+  colorGray10: 'hsl(210, 36%, 96%)',
+
+  colorSupportCyan1: 'hsl(184, 91%, 17%)',
+  colorSupportCyan2: 'hsl(185, 84%, 25%)',
+  colorSupportCyan3: 'hsl(185, 81%, 29%)',
+  colorSupportCyan4: 'hsl(184, 77%, 34%)',
+  colorSupportCyan5: 'hsl(185, 62%, 45%)',
+  colorSupportCyan6: 'hsl(360, 64%, 55%)',
+  colorSupportCyan7: 'hsl(360, 71%, 66%)',
+  colorSupportCyan8: 'hsl(360, 77%, 78%)',
+  colorSupportCyan9: 'hsl(360, 82%, 89%)',
+  colorSupportCyan10: 'hsl(360, 100%, 97%)',
+
+  colorSupportRed1: 'hsl(360, 92%, 20%)',
+  colorSupportRed2: 'hsl(360, 85%, 25%)',
+  colorSupportRed3: 'hsl(360, 79%, 32%)',
+  colorSupportRed4: 'hsl(360, 72%, 38%)',
+  colorSupportRed5: 'hsl(360, 67%, 44%)',
+  colorSupportRed6: 'hsl(360, 64%, 55%)',
+  colorSupportRed7: 'hsl(360, 71%, 66%)',
+  colorSupportRed8: 'hsl(360, 77%, 78%)',
+  colorSupportRed9: 'hsl(360, 82%, 89%)',
+  colorSupportRed10: 'hsl(360, 100%, 97%)',
+}
+
+export default colors;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Color themes
+
+Some color themes for [Styled components](https://www.styled-components.com/) and [Stylus](http://stylus-lang.com/)
+
+## Blue / yellow
+
+This themes primary colors are blue</span> and yellow.
+
+## Yellow (vivid) / light blue
+
+This themes primary colors are vivid yellow and light blue.
